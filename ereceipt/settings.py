d0e07057da63f668272df48c593752cd65fef8db
choudharyamit26 <ravichoudhary766@gmail.com>
2021-03-18 23:53:37 +0530

    Modified pos-order

--- ereceipt/settings.py
+++ ereceipt/settings.py
@@ -47,12 +47,10 @@ INSTALLED_APPS = [
     'fcm_django',
     'storages',
     'django_extensions',
-    # 'django_hosts'
 
 ]
 
 MIDDLEWARE = [
-    'django_hosts.middleware.HostsRequestMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,13 +58,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_hosts.middleware.HostsResponseMiddleware'
 ]
 
 ROOT_URLCONF = 'ereceipt.urls'
-# ROOT_HOSTCONF = 'ereceipt.hosts'  # Change `mysite` to the name of your project
-# DEFAULT_HOST = 'www'
-# PARENT_HOST = 'fatortech.net'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
