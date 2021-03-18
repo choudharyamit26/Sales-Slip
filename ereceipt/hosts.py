199a9f37e5201700e45e22be136857c205ae98e5
choudharyamit26 <ravichoudhary766@gmail.com>
2021-03-18 23:28:48 +0530

    Modified pos-order

--- ereceipt/hosts.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django_hosts import patterns, host
-
-host_patterns = patterns('',
-                         host(r'www', settings.ROOT_URLCONF, name='www'),
-                         host(r'admin', 'adminpanel.urls', name='admin'),
-                         host(r'merchant', 'merchant.urls', name='merchant'),
-                         )
