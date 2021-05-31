e1452e9eda62b0d97e72727dbfb38716dc1f0391
choudharyamit26 <ravichoudhary766@gmail.com>
2021-05-31 15:24:59 +0530

    modified webhook response

--- src/views.py
+++ src/views.py
@@ -2029,7 +2029,7 @@ class FoodicsWebHookUrl(APIView):
         return Response({'message': 'Success', 'status': HTTP_200_OK})
 
     def post(self, request, *args, **kwargs):
-        print('From foodics web hook', self.request.POST)
+        # print('From foodics web hook', self.request.POST)
         return Response({'status': HTTP_200_OK})
 
 
