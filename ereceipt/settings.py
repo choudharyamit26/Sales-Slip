0456abe1637454bd47aa80614cfe06b13e9953fa
choudharyamit26 <ravichoudhary766@gmail.com>
2021-07-24 11:32:50 +0530

    Added receipt generation from foodics api and twilio for sending messages

--- ereceipt/settings.py
+++ ereceipt/settings.py
@@ -86,24 +86,24 @@ WSGI_APPLICATION = 'ereceipt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# E-Receipt Production Database
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'ereceipt',
-#         'USER': 'ereceipt_user',
-#         'PASSWORD': 'Test@2020',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+# E-Receipt Production Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ereceipt',
+        'USER': 'ereceipt_user',
+        'PASSWORD': 'Test@2020',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
